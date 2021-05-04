339cf043d78c4fb77576816cfef7f18b574f561f
Andy James <andrewj@ksharp.net>
2021-05-04 20:52:47 +0100

    Switch to clip when not live

--- src/index.js
+++ src/index.js
@@ -5,9 +5,21 @@ import "./Componentisation";
 import "./InisUI";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const twitchEmbed = document.querySelector(".twitch-embed");
-    twitchEmbed.addEventListener("twitch-stream.offline", () => {
-        twitchEmbed.embed.setVideo("984880580");
+    
+    const liveStream = document.getElementById("live-stream");
+    const clipStream = document.getElementById("clip-stream");
+    
+    liveStream.addEventListener("twitch-stream.online", () => {
+        // twitchEmbed.embed.setVideo("TalentedAttractiveOtterNomNom-wMStXQ8bk5J4_wOn");
+        console.log("Online!");
+        liveStream.style.display = "block";
+        clipStream.style.display = "none";
+    });
+    
+    liveStream.addEventListener("twitch-stream.offline", () => {
+        // twitchEmbed.embed.setVideo("TalentedAttractiveOtterNomNom-wMStXQ8bk5J4_wOn");
         console.log("Offline!");
+        clipStream.style.display = "block";
+        liveStream.style.display = "none";
     });
 })
